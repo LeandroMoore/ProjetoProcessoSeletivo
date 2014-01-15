c2158f18ffaf3e66e33d78a1a6193081039b9998
LeandroMoore <leandromoore@hotmail.com>
2014-01-14 23:40:22 -0200

    Primeiro Commit

---
+++ ProjetoProcessoSeletivo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoProcessoSeletivo.Startup))]
+namespace ProjetoProcessoSeletivo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
